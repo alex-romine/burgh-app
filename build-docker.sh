e53b6e1339c82a90f0bf62304b6404bb6900ca5b
Alex Romine <alexromine3@gmail.com>
2021-09-19 07:02:25 +0000

    change build-docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 echo 'building image'
-docker build . -t golang:1 && echo 'running image' && docker run -it -p 8888:8888 -v $PWD:/app-dev golang:1
+docker build . -t aromine2/burgh:1 && echo 'running image' && docker run -it -p 8888:8888 -v $PWD:/app-dev aromine2/burgh:1
 
 echo 'chowning'
 sudo chown $USER:$USER -R $PWD
