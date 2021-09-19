33b074a44f1e609bb72c5034a4f6ad6f8336a2b0
Alex Romine <alexromine3@gmail.com>
2021-09-19 00:34:57 -0600

    working

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,5 @@
-docker build . -t golang:1 && docker run -it -v $PWD:/app-dev golang:1
+echo 'building image'
+docker build . -t golang:1 && echo 'running image' && docker run -it -p 8888:8888 -v $PWD:/app-dev golang:1
+
+echo 'chowning'
 sudo chown $USER:$USER -R $PWD
